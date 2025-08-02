f6c7829ea2745f1daa2b17014463b29df4584da4
Muhammed Talha Bulut <muhammed213219@ogr.duzce.edu.tr>
2025-08-02 16:20:58 +0300

    Delete frontend directory

--- frontend/src/lib/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { apiClient } from './client';
-export { authApi } from './auth';
-export { productsApi } from './products';
-export { categoriesApi } from './categories';
-export { cartApi } from './cart';
-export { ordersApi } from './orders';
